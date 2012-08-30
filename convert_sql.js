2a3e83e383d48310ed42cb227fca36459a0c8b8e
Kyle Mulka <repalviglator@yahoo.com>
2012-08-30 01:52:52 -0400

    buffering data to store multiple data chunks until the end

--- convert_sql.js
+++ convert_sql.js
@@ -2,16 +2,16 @@ var stdin = process.openStdin();
 
 stdin.setEncoding('utf8');
 
+var data = '';
+
 stdin.on('data', function (chunk) {
+	data += chunk;
+});
+
+stdin.on('end', function () {
 	var parser = require('./sql');
 
-	output = parser.parse(chunk);
+	output = parser.parse(data);
 
 	process.stdout.write(output);
 });
-
-/*
-stdin.on('end', function () {
-  process.stdout.write('end');
-});
-*/
